87b2965a8d955271163da759bd4dd59ff6d7845a
mnbinsaeed <152981544+mnbinsaeed@users.noreply.github.com>
2024-05-26 13:55:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on('interactionCreate', async (interaction) => {
     await member.voice.setChannel(jailChannel);
     await member.voice.setMute(true);
     jailedUsers.add(member.id);
-    await interaction.reply(`${member.user.tag} has been Msjon.`);
+    await interaction.reply(`${member.user.tag} has been Msjoon.`);
   } else if (commandName === 'unsjn') {
     const member = options.getMember('target');
     if (jailedUsers.has(member.id)) {
